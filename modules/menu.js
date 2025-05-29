8c2e8e3ad3a3fedff2f0fd5119da2a51902981b5
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-05-29 13:56:04 -0600

    Update menu.js

--- modules/menu.js
+++ modules/menu.js
@@ -1,1 +1,12 @@
-// Controlador de submenús
+// modules/menu.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".main-buttons button");
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const name = btn.innerText.toLowerCase().replace(" ", "");
+      window.location.href = `modules/${name}/index.html`;
+    });
+  });
+});
