2795919f3a623d05830e3b0d0f818083459f284a
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-06-01 11:00:28 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+export const API = {
+  // Scraping de Instagram (Docker backend)
+  instagram: "https://kraveai-backend-docker.onrender.com",
+
+  // IA, voz y Bitly (backend original)
+  chat: "https://kraveai-backend.onrender.com",
+  voz: "https://kraveai-backend.onrender.com",
+  bitly: "https://kraveai-backend.onrender.com",
+};
