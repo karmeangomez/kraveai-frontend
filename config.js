3d6fda32769084885c3b995f928b6851842c1608
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-06-08 16:19:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 // config.js – Versión oficial para Railway
+
 window.API = {
   instagram: "https://kraveai-backend.up.railway.app",
   chat: "https://kraveai-backend.up.railway.app",
   voz: "https://kraveai-backend.up.railway.app",
-  bitly: "https://kraveai-backend.up.railway.app"
+  bitly: "https://kraveai-backend.up.railway.app",
+  crearCuenta: "https://kraveai-backend.up.railway.app/crear-cuenta",
+  crearLote: "https://kraveai-backend.up.railway.app/create-accounts"
 };
