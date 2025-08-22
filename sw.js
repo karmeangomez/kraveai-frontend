6ac14c2caf54d45484ea631ccf953b234ce4df05
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-08-22 17:47:27 -0600

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,8 @@
-const CACHE_NAME = 'kraveai-cache-v1';
-const URLS_TO_CACHE = [
-  'index.html',
-  'styles.css',
-  'manifest.json',
-  'assets/icons/icon-192.png',
-  'assets/icons/icon-512.png'
-];
-
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
-  );
+  self.skipWaiting();
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
 self.addEventListener('activate', (event) => {
-  const whitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (!whitelist.includes(key)) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
+  event.waitUntil(self.clients.claim());
 });
+// Tener un fetch handler activo ayuda a cumplir criterios de instalación
+self.addEventListener('fetch', () => {});
