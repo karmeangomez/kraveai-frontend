bd75a738392916ffb15b7adf04e18b508e60ea7a
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-08-23 09:30:07 -0600

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', () => {}); // cumplir criterios de PWA
+// sw.js (mínimo para PWA + cache básico)
+const CACHE = "kraveai-v1";
+const CORE = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/icons/robot-192.png",
+  "/icons/robot-512.png",
+  "/icons/robot-maskable.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
